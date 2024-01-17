357e9c71c4ad8347db5934dd3d7d51cc5457b0ae
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2024-01-16 20:47:49 -0500

    Update get gif

--- src/getGif.ts
+++ src/getGif.ts
@@ -1,6 +1,6 @@
 export async function getGif(apiKey){
     const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=capybara`);
     const data = await response.json();
-    const gifUrl = data.data.embed_url;
+    const gifUrl = data.data.image.fixed_height;
     return gifUrl
 }
