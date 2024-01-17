f9f6319ede56cf89a1b0718e2abcc6c167de8d08
Vincent (Wen Yu) Ge <vincent@appwrite.io>
2024-01-17 11:55:42 -0500

    use readable streams again

--- src/form.jsx
+++ src/form.jsx
@@ -1,10 +1,16 @@
-export function DisplayGif({ gifUrl }) {
+import { renderToReadableStream } from "react-dom/server";
+
+function DisplayGif({ gifUrl }) {
     return (
         <img src={gifUrl} alt="Random Gif" />
     );
 }
 
-// export async function render(gifUrl){
-//     const stream = await renderToReadableStream(<DisplayGif gifUrl={gifUrl} />);
-//     return stream;
-// }
+export async function render(gifUrl){
+    const stream = await renderToReadableStream(
+    <div>
+        <DisplayGif gifUrl={gifUrl} />
+        <h2>Capybara of the day!</h2>
+    </div>);
+    return stream;
+}
